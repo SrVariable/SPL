6e8a18ba7b9d0d53313c2335b8f0fc30e97db3b2
SrVariable <rojohnibana@gmail.com>
2025-12-23 15:48:16 +0100

    feat(user): Implement GetUser and GetUserPlaylists

--- main.go
+++ main.go
@@ -1,26 +1,111 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/SrVariable/SPL/auth"
 	"github.com/SrVariable/SPL/config"
 	"github.com/SrVariable/SPL/tools"
 )
 
+type User struct {
+	ID string `json:"id"`
+	DisplayName string `json:"display_name"`
+}
+
+func GetUser(at *auth.AccessToken) (*User, error) {
+	endpoint := "https://api.spotify.com/v1/me"
+	req, err := http.NewRequest(
+		http.MethodGet,
+		endpoint,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", at.AccessToken))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var user User
+	if err := json.NewDecoder(resp.Body).Decode(&user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+type PlaylistTracks struct {
+	Total int `json:"total"`
+}
+type Playlist struct {
+	ID string `json:"id"`
+	Name string `json:"name"`
+	Tracks PlaylistTracks `json:"tracks"`
+}
+
+type PlaylistResponse struct {
+	Items []Playlist `json:"items"`
+}
+
+func (u *User) GetPlaylists(at *auth.AccessToken) ([]Playlist, error) {
+	endpoint := "https://api.spotify.com/v1/me/playlists"
+	req, err := http.NewRequest(
+		http.MethodGet,
+		endpoint,
+		nil,
+	)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", at.AccessToken))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result PlaylistResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+
+	return result.Items, nil
+}
+
+func (u *User) SelectPlaylist(at *auth.AccessToken) {
+	playlists, err := u.GetPlaylists(at)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Your playlists")
+	for i, playlist := range playlists {
+		fmt.Println(i + 1, playlist.Name, playlist.Tracks.Total)
+	}
+	fmt.Print("Select the playlist: ")
+}
+
 func main() {
 	env, err := config.NewEnv()
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
 	state, err := tools.GenerateCode(60)
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
 	codeVerifier, err := tools.GenerateCodeVerifier()
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
@@ -43,4 +128,12 @@ func main() {
 	}
 
 	accessToken.Save()
+
+	user, err := GetUser(accessToken)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	user.SelectPlaylist(accessToken)
 }
